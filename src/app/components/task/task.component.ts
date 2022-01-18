fa5ae432b49589f074aed0e60bf2b88d99aa76b0
David Bronfen <davidbronfen@gmail.com>
2022-01-18 09:05:25 +0200

    simple component setup - fix (#2)
    Co-authored-by: David Bronfen <davidb@carbyne911.com>

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+import {Task} from '../../models/task.model';
 
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.css']
 })
-export class TaskComponent implements OnInit {
+export class TaskComponent {
+  @Input() task: Task;
 
-  constructor() { }
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onPinTask = new EventEmitter<Event>();
 
-  ngOnInit(): void {
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onArchiveTask = new EventEmitter<Event>();
+
+  /**
+   * Component method to trigger the onPin event
+   * @param id string
+   */
+  onPin(id: any): void {
+    console.log(id);
+    this.onPinTask.emit(id);
   }
 
+  /**
+   * Component method to trigger the onArchive event
+   * @param id string
+   */
+  onArchive(id: any): void {
+    this.onArchiveTask.emit(id);
+  }
 }
